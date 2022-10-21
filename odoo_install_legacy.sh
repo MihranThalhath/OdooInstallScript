88b14dfd3a96f104884eb9fd156a478f690c7dfb
Yenthe Van Ginneken <yenthevg@gmail.com>
2022-10-21 14:48:12 +0530

    [IMP] odoo_install: include python3-cffi for Ubuntu 22.04

--- odoo_install_legacy.sh
+++ odoo_install_legacy.sh
@@ -78,7 +78,7 @@ sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
 # Install Dependencies
 #--------------------------------------------------
 echo -e "\n--- Installing Python 3 + pip3 --"
-sudo apt install git python3 python3-pip build-essential wget python3-dev python3-venv python3-wheel libxslt-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less libjpeg-dev gdebi xfonts-base xfonts-75dpi -y
+sudo apt install git python3 python3-pip python3-cffi build-essential wget python3-dev python3-venv python3-wheel libxslt-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less libjpeg-dev gdebi xfonts-base xfonts-75dpi -y
 sudo apt install libpng12-0 -y
 
 echo -e "\n---- Installing nodeJS NPM and rtlcss for LTR support ----"
